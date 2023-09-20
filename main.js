fb3a64a193a33c2dab7535b99315b15a5ad7d54a
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-09-20 10:55:14 -0700

    AIDS

---
+++ main.js
@@ -0,0 +1,7 @@
+var age = 0;
+var jobpay = 0;
+var money = 0;
+var AIDS = false; //at age up give a 1% chance of AIDS
+document.getElementById("agetrackerthing").innerHTML = "Age: "+age; //i permentantly borrowed varunans code
+
+
